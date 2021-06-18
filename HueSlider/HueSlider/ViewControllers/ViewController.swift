b2e0afb8b944caeb1287fb68b1070f9ee5b96fb4
Aleksander <aleksander.shigin@all.me>
2021-06-18 18:15:08 +0300

    created project

---
+++ HueSlider/HueSlider/ViewControllers/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  HueSlider
+//
+//  Created by Alexander on 18.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
