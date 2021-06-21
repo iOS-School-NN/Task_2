4edf9e9e1734d80f378bfe546aeb6f16eee4e0b0
kirreal <kirrealdev@gmail.com>
2021-06-22 00:45:59 +0300

    create a project

---
+++ Task_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task_2
+//
+//  Created by KirRealDev on 22.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
