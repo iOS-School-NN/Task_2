5233e3df9b6eee1ce7d5fedc7ea7f944b713b79a
ple-d <ple_dim@icloud.com>
2021-06-29 19:19:28 +0300

    minor changes

--- task2.epam.ios/task2.epam.ios/MyRainbowView.swift
+++ task2.epam.ios/task2.epam.ios/MyRainbowView.swift
@@ -19,44 +19,44 @@ class MyRainbowView: UIView {
     override func awakeFromNib() {
         super.awakeFromNib()
         createHUE()
-        let mySlider = ThumbView(frame: self.bounds)
-        addSubview(mySlider)
-        mySlider.onValueChange = { [unowned self] hueValue in self.delegate?.colorSlider(slider: mySlider, hueValue: hueValue)
+        let thumbView = ThumbView(frame: self.bounds)
+        addSubview(thumbView)
+        thumbView.onValueChange = { [unowned self] hueValue in self.delegate?.colorSlider(slider: thumbView, hueValue: hueValue)
         }
     }
     override init(frame: CGRect) {
         super.init(frame: frame)
         createHUE()
-        let mySlider = ThumbView(frame: self.bounds)
-        addSubview(mySlider)
-        mySlider.onValueChange = { [unowned self] hueValue in self.delegate?.colorSlider(slider: mySlider, hueValue: hueValue)
+        let thumbView = ThumbView(frame: self.bounds)
+        addSubview(thumbView)
+        thumbView.onValueChange = { [unowned self] hueValue in self.delegate?.colorSlider(slider: thumbView, hueValue: hueValue)
         }
+        
     }
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         createHUE()
-        let mySlider = ThumbView(frame: self.bounds)
-        addSubview(mySlider)
-        mySlider.onValueChange = { [unowned self] hueValue in self.delegate?.colorSlider(slider: mySlider, hueValue: hueValue)
+        let thumbView = ThumbView(frame: self.bounds)
+        addSubview(thumbView)
+        thumbView.onValueChange = { [unowned self] hueValue in self.delegate?.colorSlider(slider: thumbView, hueValue: hueValue)
         }
     }
-        
     
-    let colors = Array(0...359).map {
-        UIColor(hue: CGFloat($0)/359.0, saturation: 1.0, brightness: 1.0, alpha: 1.0).cgColor
-    }
-    let sliderLocation = Array(0...359).map {
-        NSNumber(value: Float($0)/(359.0))
-    }
     
     func createHUE() {
         let gradient = CAGradientLayer()
-               gradient.frame = self.bounds
-               gradient.colors = colors
-               gradient.locations = sliderLocation
-               gradient.startPoint = CGPoint(x: 0.0, y: 0)
-               gradient.endPoint = CGPoint(x: 1.0, y: 0)
-               self.layer.addSublayer(gradient)
+        let colors = Array(0...359).map {
+            UIColor(hue: CGFloat($0)/359.0, saturation: 1.0, brightness: 1.0, alpha: 1.0).cgColor
+        }
+        let sliderLocation = Array(0...359).map {
+            NSNumber(value: Float($0)/(359.0))
+        }
+        gradient.frame = self.bounds
+        gradient.colors = colors
+        gradient.locations = sliderLocation
+        gradient.startPoint = CGPoint(x: 0.0, y: 0)
+        gradient.endPoint = CGPoint(x: 1.0, y: 0)
+        self.layer.addSublayer(gradient)
     }
 
 }
